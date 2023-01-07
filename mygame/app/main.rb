37144f9c51a27a4cf89a67c68d4a3f7adad51e49
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-01-07 18:45:49 +0900

    Add bg color

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,5 +1,18 @@
 def tick(args)
-  args.outputs.labels  << [640, 500, 'Hello World!', 5, 1]
-  args.outputs.labels  << [640, 460, 'Go to docs/docs.html and read it!', 5, 1]
-  args.outputs.labels  << [640, 420, 'Join the Discord! https://discord.dragonruby.org', 5, 1]
+  render(args.outputs, args.state)
+end
+
+def render(gtk_outputs, state)
+  gtk_outputs.background_color = PALETTE[:green].to_a
+end
+
+# Dawnbringer 32 color palette
+PALETTE = {
+  bright_green: { r: 0x99, g: 0xe5, b: 0x50 },
+  green: { r: 0x6a, g: 0xbe, b: 0x30 }
+}
+
+PALETTE.each_value do |color|
+  color.define_singleton_method(:to_a) { [r, g, b] }
+  color.freeze
 end
