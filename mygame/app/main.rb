e27e8f5e6cc5a2eea19a23b1b029b0457c86abd2
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2023-01-07 19:40:36 +0900

    Render a villager

--- mygame/app/main.rb
+++ mygame/app/main.rb
@@ -1,11 +1,40 @@
 require 'smaug.rb'
 
 def tick(args)
+  setup(args) if args.tick_count.zero?
   render(args.outputs, args.state)
+end
+
+def setup(args)
+  args.state.villagers = [Villager.build(x: 20, y: 20)]
+end
+
+module Villager
+  class << self
+    def build(x:, y:)
+      @animations ||= Animations::AsespriteJson.read('sprites/villager.json')
+      {
+        x: x,
+        y: y,
+        sprite: AnimatedSprite.build(animations: @animations)
+      }
+    end
+  end
 end
 
 def render(gtk_outputs, state)
   gtk_outputs.background_color = PALETTE[:green].to_a
+  screen = gtk_outputs[:screen]
+  screen.width = 320
+  screen.height = 180
+  state.villagers.each do |villager|
+    sprite = villager[:sprite]
+    sprite[:x] = villager[:x] - 3
+    sprite[:y] = villager[:y]
+    AnimatedSprite.perform_tick(sprite, animation: :walk)
+    screen.primitives << sprite
+  end
+  gtk_outputs.primitives << { x: 0, y: 0, w: 1280, h: 720, path: :screen }.sprite!
 end
 
 # Dawnbringer 32 color palette
@@ -18,3 +47,5 @@ PALETTE.each_value do |color|
   color.define_singleton_method(:to_a) { [r, g, b] }
   color.freeze
 end
+
+$gtk.reset
